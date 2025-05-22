9ddc26f3deeb0aa3bce064f60efa4177f61f1b46
Pranav <lifeispranav@gmail.com>
2025-05-22 22:50:48 +0530

    Feat: Added non implemented msg in features

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,21 +1,31 @@
 function addProblem(req, res){
-
+  return res.status(501).json({
+    msg: "Not Implemented!"
+  })
 }
 
 function getProblem(req, res){
-
+  return res.status(501).json({
+    msg: "Not Implemented!"
+  })
 }
 
 function getProblems(req, res){
-
+  return res.status(501).json({
+    msg: "Not Implemented!"
+  })
 }
 
 function deleteProblem(req, res){
-
+  return res.status(501).json({
+    msg: "Not Implemented!"
+  })
 }
 
 function updateProblem(req, res){
-
+  return res.status(501).json({
+    msg: "Not Implemented!"
+  })
 }
 
 function pingProblemController(req, res){
